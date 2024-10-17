cd6d8fbfd0c348ff50da432330765b48a7607f68
ethan639 <139851202+ethan639@users.noreply.github.com>
2024-10-17 18:37:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '412960e6-8c6c-4190-80c4-7819647fa9f9';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
 
